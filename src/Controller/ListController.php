33ffe2a5ceb973fdb52c661179696b7302cf6362
emorales <emorales@kdoce.cl>
2022-01-13 21:54:17 +0100

    [remove] default route in ListController

--- src/Controller/ListController.php
+++ src/Controller/ListController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-#use Symfony\Component\Routing\Annotation\Route;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Controller\Annotations\QueryParam;
 
