b7e50a9ca99f9f511c15e19dff186df33bc683ff
spiderbbc <spiderbbc@gmail.com>
2022-01-14 10:13:15 +0100

    [request] creating ApiClient class

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,6 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
---
+++ src/Entity/ApiClient.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Entity;
+
+use GuzzleHttp\Client;
+
+class ApiClient
+{
+    private $_client;
+
+    public function __construct($base_uri)
+    {
+        $this->_client = new Client([
+            // Base URI is used with relative requests
+            'base_uri' => $base_uri,
+        ]);
+    }
+
+    /**
+     * getByParams: return the response of type get by params 
+     *
+     * @param string  $endpoint  endpoint for base_uri
+     * @param array   $params params for query
+     * 
+     * @return mixed
+     */ 
+    public function getByParams($endpoint,$params = [])
+    {
+        $response = $this->_client->request('GET', $endpoint,[
+            'query' => $params
+        ]);
+        $response->getHeaderLine('application/json');
+        return $response->getStatusCode() == 200 ? $response->getBody() : [];
+    }
+    
+}
