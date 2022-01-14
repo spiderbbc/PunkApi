c2e6b0a17262b5b634404a6274a396ea08903f7a
spiderbbc <spiderbbc@gmail.com>
2022-01-14 19:15:18 +0100

    [documentation] adding documentation methods PunkApi class

--- src/Entity/PunkApi.php
+++ src/Entity/PunkApi.php
@@ -4,10 +4,20 @@ namespace App\Entity;
 
 use App\Entity\ApiClient;
 
+/**
+ * PunkApi: class that wraps calls to the PunkApi api
+ *
+ */ 
 class PunkApi
 {
     private $_base_uri = 'https://api.punkapi.com';
 
+    /**
+     * getBeersByParams: get Beers By Params to api
+     * 
+     * @param array $params
+     * @return array $results
+     */
     public function getBeersByParams($params): Array
     {
         $client = new ApiClient($this->_base_uri);
@@ -15,6 +25,12 @@ class PunkApi
         return $response;
     }
 
+    /**
+     * formatString: format string change white space to underscore
+     * 
+     * @param array $params
+     * @return array $results
+     */
     public function formatString($str):string
     {
         return preg_replace('/\s+/', '_', $str);
